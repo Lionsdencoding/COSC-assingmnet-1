4a5262de24bb04bb0514de4638b9b71c46732631
Lionsdencoding <Dan.HigginsschoolEmailforschool@gmail.com>
2026-09-06 17:48:39 -0600

    Add predictions and reasoning for results in 1_Predict.py

--- 1_Predict.py
+++ 1_Predict.py
@@ -15,13 +15,15 @@ result_2 = num_items * (price_per_item + shipping_cost)
 
 # --- Your Prediction ---
 #
-# `result_1` Value:
-# `result_1` Data Type:
-# Reasoning:
+# `result_1` Value: 77.5
+# `result_1` Data Type: float
+# Reasoning: the reason why i got these predictions is because 15 x 4.5 = 67.5 +10 =77.5.
+# Then i got the data type from the data types being used being: float * int=float then, float + int=float, and if float is in the equation it will become a float type.
 #
-# `result_2` Value:
-# `result_2` Data Type:
-# Reasoning:
+# `result_2` Value: 217.5
+# `result_2` Data Type: float
+# Reasoning: the reason why i got these predictions is because (10 + 4.5)= 14.5 15 * 14.5 =217.5.
+# Then i got the data type from the data types being used being: (float + int)=float then, float * int = float, and if float is in the equation it will become a float type.
 
 
 
@@ -41,6 +43,7 @@ seconds = total_seconds % 60
 
 # --- Your Prediction ---
 #
-# `minutes` Value:
-# `seconds` Value:
-# Reasoning:
+# `minutes` Value: 6
+# `seconds` Value: 35
+# Reasoning: // is a division so 395 divided by 60 would equal 6. We can also check this by multiplying 60 x 6 = 360.
+# then for the seconds we can take 395 and do % 60 which would equal 35 which is the remainder of 395/60
