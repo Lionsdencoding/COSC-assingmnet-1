cfcc36f727e7c58d873a6405dbe3129a5d64acff
Lionsdencoding <Dan.HigginsschoolEmailforschool@gmail.com>
2026-09-06 18:13:13 -0600

    Revise answers in 2_Investigate.py
    Updated answers for operator precedence, data types, and math module questions.

--- 2_Investigate.py
+++ 2_Investigate.py
@@ -22,17 +22,18 @@ print(f"Result 2 is: {result_2}, Type: {type(result_2)}")
 #     the multiplication (`*`) or the addition (`+`)? Why does Python
 #     choose this order? (This is called "operator precedence").
 #
-#     Your Answer:
+#     Your Answer: The operation that happens first is multiplication. This is because the operation gets executed based on the order of operation going from
+#     parenthesis, exponents, multiplication/division, add/subtract from left to right on the code line, with multiplication happening before addition because of its operator precedence.
 #
 # 2.  What is the data type of `result_1`? Why is it that type, even though
 #     `num_items` and `shipping_cost` are integers?
 #
-#     Your Answer:
+#     Your Answer: The data type of result_1 is float. The reason why it is float is because, even if integers are there, if the type float is present the outcome will become a float automatically.
 #
 # 3.  How do the parentheses `()` in the calculation for `result_2` change
 #     the order of operations compared to `result_1`?
 #
-#     Your Answer:
+#     Your Answer: The parentheses in Result_2 change the order of operations by overiding the usual left to right order to instead do the parathesis first then do revert back to left to right.
 
 
 # PRIMM: Investigate Activity
@@ -53,14 +54,16 @@ print(f"The area of a circle with radius {radius} is {area}")
 # 1.  What does the `import math` line do? What do you predict would happen
 #     if you deleted that line and tried to run the code?
 #
-#     Your Answer:
+#     Your Answer:The import math line imports the math module of python allowing for commands like math.pi to be available.
+#     if import math were to be removed then the code would instead bring up an error because the pi command wouldn't work.
 #
 # 2.  What is `math.pi`? Based on how it's used, do you think it is a
 #     function or a variable defined inside the `math` module?
 #
-#     Your Answer:
+#     Your Answer:math.pi is a function that is made to represent the pi symbol in math. The Variable math.pi is a variable because it has no parentheses.
 #
 # 3.  In the expression `math.pi * radius ** 2`, which operation happens first:
 #     the multiplication (`*`) or the exponentiation (`**`)? How could you confirm this?
 #
-#     Your Answer:
+#     Your Answer: The exponent ** happens first because it it higher on the order of operations than multiplication(*).
+#     You can confirm this by using parantheses on radius ** 2 to get the same result or by doing a simple equation such as 3 * 10**2 and seeing the results
