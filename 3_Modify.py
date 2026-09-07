ed97dd3d1773f91dc8bebd32f0f63e0820697577
Lionsdencoding <Dan.HigginsschoolEmailforschool@gmail.com>
2026-09-06 18:54:57 -0600

    Update 3_Modify.py

--- 3_Modify.py
+++ 3_Modify.py
@@ -15,20 +15,20 @@ print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
 # Change the formula to convert from a starting `fahrenheit` value of 77 to `celsius`.
 # The formula for that is `(fahrenheit - 32) * 5/9`.
 #
-# Prediction:
-# Verified Result: (What does the program print?)
+# Prediction: The result from (77 - 32) * 5/9 will equal the 25 celcius in the original source code because you are reversing the operation
+# Verified Result: the program prints that the 77 degrees fahrenheit does indeed equal 25 degrees cecius when the equation takes place
 
 
 # --- Modification 2 ---
 # Go back to the original code. Change the `celsius` variable to `25.5`.
 #
-# Prediction: What will the new Fahrenheit value be? Will the data type of the `fahrenheit` variable change?
-# Verified Result:
+# Prediction: What will the new Fahrenheit value be? The fahrenheit value will 77.9 or higher. Will the data type of the `fahrenheit` variable change? No the data type will not change as it is still a float type by "/" being used
+# Verified Result: The Fahrenheit value did increase to 77.9 and the data type did reamin the same.
 
 
 # --- Modification 3 ---
 # Go back to the original code. Try to "break" the program by changing the calculation
 # to `fahrenheit = (celsius * "9/5") + 32`. Notice the quotes around `9/5`.
 #
-# Prediction: What kind of error do you think this will cause and why?
-# Verified Result:
+# Prediction: What kind of error do you think this will cause and why? This will most likely cause a type error due to incompatible types being used.
+# Verified Result: A type error did occur by the error message saying that an int type cant be multipied by a string(the "9/5")
